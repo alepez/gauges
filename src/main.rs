8cbfd76748f0e2b6de2aba4d256f917c4d1b1be3
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-03-17 00:56:37 +0100

    Integrate tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::cell::Cell;
+use std::net::SocketAddr;
+use std::str::FromStr;
 use std::time::Duration;
 
 use dioxus::prelude::dioxus_hot_reload::Config as HotReloadConfig;
@@ -6,10 +8,13 @@ use dioxus::prelude::hot_reload_init;
 use dioxus_desktop::Config as DesktopConfig;
 use gauges::{app, AppProps};
 use gauges::{GaugeId, GaugeProps, GaugeStyle, Range};
+use tokio::net::TcpListener;
 use tokio::sync::mpsc::unbounded_channel;
+use tokio_stream::StreamExt;
+use tokio_util::codec::{Framed, LinesCodec};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     hot_reload_init!(HotReloadConfig::new().with_rebuild_command("cargo run"));
 
     let _gauge = GaugeProps {
@@ -22,11 +27,16 @@ async fn main() {
     let other = sender.clone();
 
     tokio::spawn(async move {
-        let mut count = 0.0;
-        loop {
-            tokio::time::sleep(Duration::from_millis(1000)).await;
-            count += 1.0;
-            other.send(count).unwrap();
+        let bind = SocketAddr::from_str("127.0.0.1:9999").unwrap();
+        let listener = TcpListener::bind(bind).await.unwrap();
+
+        let (socket, _) = listener.accept().await.unwrap();
+        let mut server = Framed::new(socket, LinesCodec::new_with_max_length(1024));
+        while let Some(Ok(line)) = server.next().await {
+            let parsed: Option<f64> = line.parse().ok();
+            if let Some(x) = parsed {
+                other.send(x).unwrap();
+            }
         }
     });
 
@@ -36,4 +46,6 @@ async fn main() {
     };
 
     dioxus_desktop::launch_with_props(app, props, DesktopConfig::default());
+
+    Ok(())
 }
