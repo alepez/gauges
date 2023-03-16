510cdb57f5058d49311e7c818c09e64e3e8adb3b
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-03-16 23:53:49 +0100

    Use lates (git) dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use dioxus::prelude::dioxus_hot_reload::Config;
+use dioxus::prelude::hot_reload_init;
 use gauges::app;
 use gauges::{GaugeId, GaugeProps, GaugeStyle, Range};
 
 fn main() {
+    hot_reload_init!(Config::new().with_rebuild_command("cargo run"));
+
     let _gauge = GaugeProps {
         id: GaugeId::from("foo".to_owned()),
         range: Range { min: 0, max: 100 },
