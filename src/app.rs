cbd2741ca84e7a0f94b78219324e0c78bc643835
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-03-19 22:09:01 +0100

    Draw svg arc

--- src/app.rs
+++ src/app.rs
@@ -16,6 +16,24 @@ pub struct AppProps {
     pub receiver: Cell<Option<Receiver>>,
 }
 
+fn arc_commands() -> String {
+    let radius = 100;
+
+    let begin_x = 50;
+    let begin_y = 150;
+    let rx = radius;
+    let ry = radius;
+    let angle = 0;
+    let larg_arc_flag = 1;
+    let sweep_flag = 0;
+    let end_x = 150;
+    let end_y = 50;
+
+    format!(
+        "M {begin_x} {begin_y} A {rx} {ry} {angle} {larg_arc_flag} {sweep_flag} {end_x} {end_y}"
+    )
+}
+
 fn app(cx: Scope<AppProps>) -> Element {
     let value = use_state(&cx, || 0.0);
     let receiver = cx.props.receiver.take();
@@ -36,9 +54,23 @@ fn app(cx: Scope<AppProps>) -> Element {
         }
     });
 
+    let commands = arc_commands();
+
     cx.render(rsx! {
         div {
             div { "{value}" }
+            div {
+                svg {
+                    width: "300px",
+                    height: "300px",
+                    path {
+                        fill: "none",
+                        stroke: "#000000",
+                        stroke_width: "20",
+                        d: "{commands}"
+                    }
+                }
+            }
         }
     })
 }
