8f30c023e7c44d56c864ef7d4977cace4da1e402
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-03-20 11:24:15 +0100

    Refactor example client

--- src/bin/example-client.rs
+++ src/bin/example-client.rs
@@ -1,23 +1,37 @@
+use gauges::core::{Record, Value};
 use std::time::Duration;
-
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 use tokio::time::sleep;
 
+struct RecordsGenerator {
+    x: f64,
+    step: f64,
+}
+
+impl RecordsGenerator {
+    fn next(&mut self) -> Record {
+        if self.x >= 100.0 {
+            self.step = -1.0;
+        }
+        if self.x <= 0.0 {
+            self.step = 1.0;
+        }
+        self.x += self.step;
+
+        Record {
+            value: Value::Float(self.x),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect("127.0.0.1:9999").await?;
-    let mut x = 0;
-    let mut step = 1;
+    let mut generator = RecordsGenerator { x: 0.0, step: 1.0 };
     loop {
-        if x == 100 {
-            step = -1
-        }
-        if x == 0 {
-            step = 1
-        }
-        x += step;
-        let s = format!("{}\n", x);
+        let x = generator.next();
+        let s = format!("{}\n", x.value);
         if stream.write(s.as_bytes()).await.is_err() {
             break;
         }
