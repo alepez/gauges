alepez/gauges

commit e27df91eaf30ce32142cb7a1aa8b328f65753e2b

Add desktop feature

src/app/dashboard.rs
@@ -45,11 +45,12 @@ pub fn Dashboard(cx: Scope<DashboardProps>) -> Element {
     let signals = &cx.props.signals;
     let items = &cx.props.config.items;
     let show_age_indicator = cx.props.age_indicator;
+    let first = items.first().unwrap();
+    let (item, value, age) = extract_info(signals, first);
 
     cx.render(rsx! {
         div {
             class: "dashboard",
-            for (item, value, age) in items.iter().map(|x| extract_info(signals, x)) {
                 Gauge {
                     value: value
                     signal: item.signal.clone(),
@@ -58,7 +59,6 @@ pub fn Dashboard(cx: Scope<DashboardProps>) -> Element {
                     format: item.format,
                     age: calculate_age(show_age_indicator, age),
                 }
-            }
         }
     })
 }

Cargo.toml
@@ -19,7 +19,7 @@ name = "gauges-server"
 [dependencies]
 angle = "0.5"
 dioxus = "0.3"
-dioxus-desktop = "0.3"
+dioxus-desktop = { version = "0.3", optional = true }
 serde = { version = "1", features = ["derive"] }
 serde_json = "1"
 tokio = { version = "1", features = ["full"] }
@@ -30,3 +30,7 @@ toml = "0.7"
 [dev-dependencies]
 futures-util = "0.3"
 tokio-socketcan = "0.3"
+
+[features]
+default = ["desktop"]
+desktop = ["dioxus-desktop"]

src/app.rs
@@ -1,117 +1,2 @@
-use std::rc::Rc;
-
-use dioxus::prelude::*;
-use dioxus_desktop::Config as DesktopConfig;
-
-use dashboard::Dashboard;
-
-use crate::core::DashboardConfig;
-use crate::core::Signals;
-use crate::net::{channel, Sender};
-
-mod dashboard;
-mod gauge;
-
-struct AppProps<F, T>
-where
-    F: Fn(Sender, String) -> T + 'static,
-    T: std::future::Future<Output = ()>,
-{
-    dashboard: Rc<DashboardConfig>,
-    launch_server: &'static F,
-}
-
-pub fn launch_app(dashboard: DashboardConfig) {
-    launch_app_with_server(dashboard, &crate::net::launch_server)
-}
-
-#[cfg(debug_assertions)]
-fn custom_head() -> String {
-    r#"<link rel="stylesheet" href="assets/style.css" />"#.to_owned()
-}
-
-#[cfg(not(debug_assertions))]
-fn custom_head() -> String {
-    format!(r#"<style>{}</style>"#, include_str!("../assets/style.css"))
-}
-
-pub fn launch_app_with_server<F, T>(dashboard: DashboardConfig, launch_server: &'static F)
-where
-    F: Fn(Sender, String) -> T + 'static,
-    T: std::future::Future<Output = ()> + 'static, // TODO Why this needs to be static?
-{
-    let window = dioxus_desktop::WindowBuilder::new().with_title("Gauges");
-
-    let config = DesktopConfig::new()
-        .with_window(window)
-        .with_custom_head(custom_head());
-
-    let dashboard = Rc::new(dashboard);
-    let props = AppProps {
-        dashboard,
-        launch_server,
-    };
-    dioxus_desktop::launch_with_props(app, props, config);
-}
-
-fn app<F, T>(cx: Scope<AppProps<F, T>>) -> Element
-where
-    F: Fn(Sender, String) -> T + 'static,
-    T: std::future::Future<Output = ()>,
-{
-    let signals = use_ref(cx, || {
-        let signals: Signals = cx.props.dashboard.as_ref().clone().into();
-        signals
-    });
-
-    let updates_count = use_state(cx, || 0);
-
-    let started = use_state(cx, || false);
-
-    let (sender, mut receiver) = channel();
-
-    // TODO Why this does not work but the other works?
-    // let launch_server = &cx.props.launch_server;
-    let launch_server: &'static F = cx.props.launch_server;
-    let addr = cx.props.dashboard.addr.clone();
-
-    if !started {
-        started.set(true);
-        cx.spawn(async move {
-            launch_server(sender.clone(), addr).await;
-        });
-    }
-
-    let _ = use_coroutine(cx, |_: UnboundedReceiver<()>| {
-        to_owned![signals];
-        async move {
-            while let Some(record) = receiver.recv().await {
-                signals.write().insert_named_record(record);
-                signals.needs_update();
-            }
-        }
-    });
-
-    let show_age_indicator = cx.props.dashboard.age_indicator;
-
-    let _ = use_coroutine(cx, |_: UnboundedReceiver<()>| {
-        to_owned![updates_count];
-        async move {
-            if show_age_indicator {
-                loop {
-                    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-                    updates_count += 1;
-                }
-            }
-        }
-    });
-
-    cx.render(rsx! {
-        Dashboard {
-            config: cx.props.dashboard.clone(),
-            signals: signals.read().clone(),
-            updates_count: *updates_count.get(),
-            age_indicator: show_age_indicator,
-        }
-    })
-}
+pub mod dashboard;
+pub mod gauge;

src/bin/gauges-server.rs
@@ -3,7 +3,7 @@ use std::f64::consts::PI;
 use angle::Rad;
 use serde::{Deserialize, Serialize};
 
-use gauges::app::launch_app;
+use gauges::desktop::launch_app;
 use gauges::prelude::*;
 
 const CIRCLE_STYLE: GaugeStyle = GaugeStyle::Circle(CircleGaugeStyle { radius: 50.0 });

src/desktop.rs
@@ -1,0 +1,114 @@
+use std::rc::Rc;
+
+use dioxus::prelude::*;
+use dioxus_desktop::Config as DesktopConfig;
+
+use crate::app::dashboard::Dashboard;
+
+use crate::core::DashboardConfig;
+use crate::core::Signals;
+use crate::net::{channel, Sender};
+
+struct AppProps<F, T>
+where
+    F: Fn(Sender, String) -> T + 'static,
+    T: std::future::Future<Output = ()>,
+{
+    dashboard: Rc<DashboardConfig>,
+    launch_server: &'static F,
+}
+
+pub fn launch_app(dashboard: DashboardConfig) {
+    launch_app_with_server(dashboard, &crate::net::launch_server)
+}
+
+#[cfg(debug_assertions)]
+fn custom_head() -> String {
+    r#"<link rel="stylesheet" href="assets/style.css" />"#.to_owned()
+}
+
+#[cfg(not(debug_assertions))]
+fn custom_head() -> String {
+    format!(r#"<style>{}</style>"#, include_str!("../assets/style.css"))
+}
+
+pub fn launch_app_with_server<F, T>(dashboard: DashboardConfig, launch_server: &'static F)
+where
+    F: Fn(Sender, String) -> T + 'static,
+    T: std::future::Future<Output = ()> + 'static, // TODO Why this needs to be static?
+{
+    let window = dioxus_desktop::WindowBuilder::new().with_title("Gauges");
+
+    let config = DesktopConfig::new()
+        .with_window(window)
+        .with_custom_head(custom_head());
+
+    let dashboard = Rc::new(dashboard);
+    let props = AppProps {
+        dashboard,
+        launch_server,
+    };
+    dioxus_desktop::launch_with_props(app, props, config);
+}
+
+fn app<F, T>(cx: Scope<AppProps<F, T>>) -> Element
+where
+    F: Fn(Sender, String) -> T + 'static,
+    T: std::future::Future<Output = ()>,
+{
+    let signals = use_ref(cx, || {
+        let signals: Signals = cx.props.dashboard.as_ref().clone().into();
+        signals
+    });
+
+    let updates_count = use_state(cx, || 0);
+
+    let started = use_state(cx, || false);
+
+    let (sender, mut receiver) = channel();
+
+    // TODO Why this does not work but the other works?
+    // let launch_server = &cx.props.launch_server;
+    let launch_server: &'static F = cx.props.launch_server;
+    let addr = cx.props.dashboard.addr.clone();
+
+    if !started {
+        started.set(true);
+        cx.spawn(async move {
+            launch_server(sender.clone(), addr).await;
+        });
+    }
+
+    let _ = use_coroutine(cx, |_: UnboundedReceiver<()>| {
+        to_owned![signals];
+        async move {
+            while let Some(record) = receiver.recv().await {
+                signals.write().insert_named_record(record);
+                signals.needs_update();
+            }
+        }
+    });
+
+    let show_age_indicator = cx.props.dashboard.age_indicator;
+
+    let _ = use_coroutine(cx, |_: UnboundedReceiver<()>| {
+        to_owned![updates_count];
+        async move {
+            if show_age_indicator {
+                loop {
+                    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                    updates_count += 1;
+                }
+            }
+        }
+    });
+
+    cx.render(rsx! {
+        Dashboard {
+            config: cx.props.dashboard.clone(),
+            signals: signals.read().clone(),
+            updates_count: *updates_count.get(),
+            age_indicator: show_age_indicator,
+        }
+    })
+}

src/lib.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "desktop")]
+pub mod desktop;
+
 pub mod app;
 pub mod core;
 pub mod net;

commit 2f5199b5af004f181fbce18870e9b49c0471fa4f

Fix multiple gauges.
Fix a regression introduced in e27df91

src/app/dashboard.rs
@@ -45,12 +45,11 @@ pub fn Dashboard(cx: Scope<DashboardProps>) -> Element {
     let signals = &cx.props.signals;
     let items = &cx.props.config.items;
     let show_age_indicator = cx.props.age_indicator;
-    let first = items.first().unwrap();
-    let (item, value, age) = extract_info(signals, first);
 
     cx.render(rsx! {
         div {
             class: "dashboard",
+            for (item, value, age) in items.iter().map(|x| extract_info(signals, x)) {
                 Gauge {
                     value: value
                     signal: item.signal.clone(),
@@ -59,6 +58,7 @@ pub fn Dashboard(cx: Scope<DashboardProps>) -> Element {
                     format: item.format,
                     age: calculate_age(show_age_indicator, age),
                 }
+            }
         }
     })
 }